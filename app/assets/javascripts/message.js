459d75b36cfbe31d888284de99f82672d7753293
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-06-20 20:30:50 +0900

    calcu-comp1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -23,15 +23,15 @@ $(function(){
     else {
      var html =
       `<div class="message">
-        <div class="upper-message">
-          <div class="upper-message__user-name">
+        <div class="message-list__message-title">
+          <div class="message-list__message-title__user-name">
             ${message.user_name}
           </div>
-          <div class="upper-message__date">
+          <div class="message-list__message-title__message-date">
             ${message.created_at}
           </div>
         </div>
-        <div class="lower-message">
+        <div class="message-list__message-content">
           <p class="lower-message__content">
             ${message.content}
           </p>
@@ -40,22 +40,22 @@ $(function(){
     return html;
     };
   }
-  $('.new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+      $('.main-chat__message-list').append(html);
+      $('.main-chat__message-list').animate({scrollTop: $('.main-chat__message-list')[0].scrollHeight}, 'fast');   
       $('form')[0].reset();
     })
     .fail(function(){
